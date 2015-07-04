00da70520e9e5ba6c72db7a8935e1963124a3572
yorisilo <yorisilo@gmail.com>
2015-07-04 11:25:52 +0900

    spec6: update recipe.rb and memo.md

--- memo.md
+++ memo.md
@@ -1,5 +1,16 @@
 # recipe.rb の使い方
 
+
+## `recipe-data.txt` の一覧表示
+
 ```ruby
-ruby recipe.sh recipe-data.txt
+ruby recipe.sh recipe-data.txt 0
+```
+
+
+## `recipe-data.txt` のidを指定してそれを表示する．
+以下の例は id が 1 の recipe を表示する．
+
+```ruby
+ruby recipe.sh recipe-data.txt 1
 ```
--- recipe.rb
+++ recipe.rb
@@ -10,4 +10,13 @@ def showRecipesId(recipes)
   end
 end
 
-showRecipesId(ARGV[0])
+def recipeId(recipes, id)
+  File.open(recipes) do |f|
+    ary = f.readlines
+    idName = ary[id-1]
+    puts "#{id}: #{idName}" if id != 0
+    showRecipesId(recipes)  if id == 0
+  end
+end
+
+recipeId(ARGV[0], ARGV[1].to_i)
